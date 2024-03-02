52f0d47735021c5dd8988dc8380ac0b1a461465a
datjandra <donny.tjandra@gmail.com>
2024-03-01 19:56:46 -1000

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ def main():
         # Form fields
         age = st.number_input("Age", min_value=0, max_value=150, step=1, value=25)
         gender = st.selectbox("Gender", ["Male", "Female", "Other"])
-        lifestyle = st.input("Lifestyle")
-        conditions = st.input("Conditions")
+        lifestyle = st.text_input("Lifestyle")
+        conditions = st.text_input("Conditions")
 
         # Translation option
         translation = st.radio("Translate to", ["Arabic", "Urdu"])
